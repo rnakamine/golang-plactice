e7b46afcdb8e3f82243f4c1dd873ce7dabcab9b9
Ryo Nakamine <rnakamine8080@gmail.com>
2021-02-20 12:06:56 +0900

    Add error handling example

---
+++ error-handling-example/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+func ToStringer(v interface{}) (Stringer, error) {
+	if i, ok := v.(Stringer); ok {
+		return i, nil
+	}
+	return nil, MyError("CastError")
+}
+
+type error interface {
+	Error() string
+}
+
+type MyError string
+
+func (e MyError) Error() string {
+	return string(e)
+}
+
+type I int
+
+func (i I) String() string {
+	return "I"
+}
+
+func main() {
+	i := I(100)
+	s, err := ToStringer(i)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(s)
+	}
+}
